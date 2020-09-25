ddf6541923ea118c99f1b942cc0433c2f62603f0
b-low <bclow@umich.edu>
2020-09-24 15:21:31 -1000

    Manual workout progress

--- FitnessTracker/Controllers/CurrentRunViewController.swift
+++ FitnessTracker/Controllers/CurrentRunViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  CurrentRunViewController.swift
 //  FitnessTracker
 //
--- FitnessTracker/Controllers/RecordWorkoutViewController.swift
+++ FitnessTracker/Controllers/RecordWorkoutViewController.swift
@@ -7,21 +7,60 @@
 //
 
 import UIKit
+import HealthKit
 
-class RecordWorkoutViewController: UIViewController {
+class RecordWorkoutViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    var workoutValues: [(HKWorkoutActivityType, String)] = [
+        (HKWorkoutActivityType.cycling, "Cycling"),
+        (HKWorkoutActivityType.running, "Running"),
+        (HKWorkoutActivityType.swimming, "Swimming"),
+        (HKWorkoutActivityType.equestrianSports, "Equestrian Sports"),
+        (HKWorkoutActivityType.other, "Other")
+    ]
+    
     // MARK: - View event handlers
     override func viewDidLoad() {
         super.viewDidLoad()
+        workoutPicker.delegate = self
+        workoutPicker.dataSource = self
     }
     
     // MARK: - IBOutlets and IBActions
     @IBOutlet var cancelButton: UIButton!
     
     @IBOutlet var startStopButton: UIButton!
+    var workoutStart: Date? = nil
+    
+    @IBOutlet weak var workoutPicker: UIPickerView!
+    
+    @IBAction func cancelWorkout(_ sender: Any) {
+        workoutStart = nil
+    }
     
     @IBAction func startStopWorkout() {
-        // TODO: Add some logic to start/stop the workout. This code is set up
-        // to use the same button for start/stop, but you could change that if
-        // you want.
+        if workoutStart == nil {
+            startStopButton.setTitle("Stop", for: UIControl.State.normal)
+            workoutStart = Date()
+        } else {
+            startStopButton.setTitle("Start", for: UIControl.State.normal)
+            let workoutType = workoutValues[workoutPicker.selectedRow(inComponent: 0)].0
+            print(HKWorkout(activityType: workoutType, start: workoutStart!, end: Date()))
+            workoutStart = nil
+            
+            // TODO: Save workout
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return workoutValues.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return workoutValues[row].1
     }
 }
